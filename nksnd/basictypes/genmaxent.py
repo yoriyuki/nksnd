d8ded79d4d0e957b087f5f20b31743a85b8426a9
yoriyuki <yoriyuki.y@gmail.com>
2016-05-29 19:58:17 +0900

    feature extraction

--- nksnd/basictypes/genmaxent.py
+++ nksnd/basictypes/genmaxent.py
@@ -1,5 +1,8 @@
 from maxent import MaxentModel
 
+def get_features(context):
+    return map(lambda data: data.key(), context)
+
 class GenMaxEntModel:
     def __init__(self):
         self.__m = MaxentModel()
@@ -20,7 +23,8 @@ class GenMaxEntModel:
         self.__begin_add_event()
         for event in events:
             context, outcome = event
-            self.__add_event(context, outcome)
+            features = get_features(context)
+            self.__add_event(features, outcome.key())
         self.__end_add_event()
         self.__train()
 
@@ -31,16 +35,20 @@ class GenMaxEntModel:
         self.__m.load(name)
 
     def eval(self, context, outcome):
-        return self.__m.eval(context, outcome)
+        return self.__m.eval(get_features(context), outcome.key())
 
-    def eval_all(self, context):
-        return self.__m.eval_all(context)
 
 def _test():
+    class Data:
+        def __init__(self, data):
+            self._data = data
+        def key(self):
+            return self._data
     model = GenMaxEntModel()
-    model.train([(['1', '2'], '3'), (['1', '3'], '4')])
-    print(model.eval(['1', '2'], '3'))
-    print(model.eval(['1', '2'], '4'))
+    model.train([([Data('1'), Data('2')], Data('3')),
+                ([Data('1'), Data('3')], Data('4'))])
+    print(model.eval([Data('1'), Data('2')], Data('3')))
+    print(model.eval([Data('1'), Data('2')], Data('4')))
 
 if __name__ == "__main__":
     _test()
