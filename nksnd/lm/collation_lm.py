f04f1e6195251151dbf6aad0295d57ddc04e2283
yoriyuki <yoriyuki.y@gmail.com>
2016-05-30 02:09:07 +0900

    argument parsing (again)

--- nksnd/lm/collation_lm.py
+++ nksnd/lm/collation_lm.py
@@ -1,6 +1,6 @@
 import genmaxent
 
-class CollationModel:
+class CollationLM:
     def __init__(self):
         self._model = GenMaxEntModel()
 
@@ -9,3 +9,23 @@ class CollationModel:
 
     def eval(self, context, outcome):
         return self._model.eval(context, outcome)
+
+    def save(self, file):
+        self._model.save(file)
+
+    def load(self, file):
+        self._model.load(file)
+
+
+if __name__ == "__main__":
+    import argparse
+    import os
+    parser = argparse.ArgumentParser(
+        description='Generate colllation language model')
+    parser.add_argument('-o', '--outdir',
+        default='../../data',
+        help='output directory: default=../../data')
+    parser.add_argument('inputs', nargs='?'
+        help='input corpus')
+    args = parser.parse_args()
+    output_file = os.path.join(args.outdir, 'collation_lm')
