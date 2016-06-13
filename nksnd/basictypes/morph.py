ccb3c6c35e8ee003452a83646a16ec23d00a3c9d
yoriyuki <yoriyuki.y@gmail.com>
2016-06-14 00:55:08 +0900

    UnknownMorph

--- nksnd/basictypes/morph.py
+++ nksnd/basictypes/morph.py
@@ -1,3 +1,5 @@
+import utils
+
 class Morph:
     def __init__(self, surface):
         self._surface = surface
@@ -9,10 +11,18 @@ class Morph:
         return ':' + self._surface
 
 class UnknownMorph(Morph):
-    def __init__(self, kind, surface):
+    def __init__(self, surface):
         super(Morph, self).__init__(surface)
-        assert kind == 'hiragana' || kind == 'katakana' || kind == 'other'
-        self._kind = kind
+        if utils.is_hiragana(surface):
+            self._kind = 'hiragana'
+        else if utils.is_katakana(surdace):
+            self._kind = 'katakana'
+        else:
+            self._kind = 'other'
+        if len(surface) <= 6:
+            self._len = len(surface)
+        else:
+            self._len = '*'
 
     def key(self):
-        return '_' + self._surface
+        return '_' + self_len + self._kind
