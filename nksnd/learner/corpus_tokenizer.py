423a2b97b768e21c370a84f7493d769ea3aa9165
yoriyuki <yoriyuki.y@gmail.com>
2016-06-14 02:45:48 +0900

    corpus_tokenizer

--- nksnd/learner/corpus_tokenizer.py
+++ nksnd/learner/corpus_tokenizer.py
@@ -1,14 +1,21 @@
 from ..basictypes import utils
+from ..basictypes import morph
 
+def _morph_from_word(word, cut_off_set):
+    if word in cut_off_set:
+        morph.Morph(word)
+    else:
+        morph.UnknownMorph(word)
 
-
-def corpus_tokenizer(file_names):
-    files = map(open, file_names)
-    counts = count_words(utils.concat(files))
+def cut_off_set(files):
+    sentences = (line.split() for line in utils.concat(files))
+    counts = count_words(sentences)
     cut_off_set = utils.cut_off_set(counts, cut_off=1)
     for f in files:
         f.close()
+    return cut_off_set
 
-    files = map(open, file_names)
-    for sentence in utils.concat(files):
-        for word in sentence.split()
+def corpus_tokenizer(files, cut_off_set):
+    sentences = (line.split() for line in utils.concat(files))
+    for sentence in sentences:
+        yield [_morph_from_word(word, cut_off_set) for word in sentence]
