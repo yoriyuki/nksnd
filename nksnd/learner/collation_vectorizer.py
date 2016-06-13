555b66df839f34a1d507b6422b086b2c3a75b5b6
yoriyuki <yoriyuki.y@gmail.com>
2016-06-14 03:44:26 +0900

    collation_vectorizer.py in progress

---
+++ nksnd/learner/collation_vectorizer.py
@@ -0,0 +1,8 @@
+class CollationVectorizer():
+    def __init__(self):
+        self._feature_num = 0
+        self._feature_id_map = {}
+        self._outcome_num = 0
+        self._outcome_id_map = {} 
+
+    def fit_transform(samples):
