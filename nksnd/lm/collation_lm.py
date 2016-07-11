6976e984d87e3b4edd6268d9edd643484ad2a922
Yoriyuki Yamagata <yoriyuki.y@gmail.com>
2016-07-11 17:55:38 +0900

    print, vectroizer in progress

--- nksnd/lm/collation_lm.py
+++ nksnd/lm/collation_lm.py
@@ -1,52 +1,11 @@
-from sklearn.linear_model import LogisticRegression
-from scipy.sparse import lil_matrix
-from numpy import array, zeros
-from sklearn.decomposition import TruncatedSVD
-from sklearn.cluster import AgglomerativeClustering
-import mmh3
-
-def _samples(sentences):
-    for sentence in sentences:
-        for i in range(len(sentence)):
-            features = set(map(lambda m: m.key(), sentence[0:i-1]))
-            yield (features, sentence[i].key())
-
-_hash_num = 2**20
-
-def _sign(n):
-    return n / abs(n)
-
-def _hash(s):
-    h = mmh3.hash(s)
-    return _sign(h) * (abs(h) % _hash_num)
-
-def _feature_vec(features):
-    return map(_hash, features)
-
-def _outcome_id(outcome):
-    return abs(_hash(outcome))
-
-def _hashed_samples(samples):
-    for features, outcome in samples:
-        yield (_feature_vec(features), _outcomd_id(outcome))
-
-def _inverse(x):
-    y = {}
-    for i in range(len(x)):
-        y[x[i]] = i
-    return y
+from ..lerner.collation_vectorizer import CollationVectorizer
 
 class CollationLM:
     def __init__(self, penalty='l2', solver='lbfgs', max_iter=10, feature_num=1000, outcome_num=1000):
         self._model = LogisticRegression(penalty=penalty, solver=solver, verbose=1, max_iter=max_iter)
         self._decomp_features = TruncatedSVD(n_components=feature_num)
-        self._n_features = _hash_num
-        self._n_outcomes = _hash_num
         self._feature_num = feature_num
         self._outcome_num = outcome_num
-
-    def _cluster_outcome(self, outcome):
-        return self._cluster_ids[self._inverse_y[outcome]]
 
     def feature_num(self):
         return self._feature_num
@@ -54,12 +13,16 @@ class CollationLM:
     def outcome_num(self):
         return self._outcome_num
 
-    def train(self, sentences):
-        print "building samples..."
+    def train(self, file_names):
+        vectorizer = CollationVectorizer(self._feature_num, self._outcome_num)
+        x = vectorizer.fit_transform(file_names)
+        
+
+        print("building samples...")
         hashed_samples = list(_hashed_samples(_samples(sentences)))
         print len(hashed_samples), "samples"
 
-        print "building the feature matrix..."
+        print("building the feature matrix...")
         raw_x = lil_matrix((len(hashed_samples), _hash_num))
         outcome_ids = []
         for i in range(len(hashed_samples)):
@@ -68,7 +31,7 @@ class CollationLM:
             for feature_id in feature_ids:
                 raw_x[i, abs(feature_id)] += _sign(feature_id)
 
-        print "compressing features..."
+        print("compressing features...")
         x = self._decomp_features.fit_transform(raw_x)
 
         print("building the outcome matrix...")
@@ -85,15 +48,15 @@ class CollationLM:
         y = y_count.keys()
         y_features = [y_feature_sums[i].toarray()[0] / float(y_count[i]) for i in y]
 
-        print "clustering outcomes..."
+        print("clustering outcomes...")
         decomp_outcomes = AgglomerativeClustering(n_clusters=self.outcome_num())
         cluster_ids = decomp_outcomes.fit_predict(y_features)
         self._cluster_ids = cluster_ids
         self._inverse_y = _inverse(y)
-        print "training..."
+        print("training...")
         self._model.fit(x, map(self._cluster_outcome, outcome_ids))
         sparcity = (self._model.coef_ == 0).sum() / float(self._model.coef_.size)
-        print "Sparcity:", sparcity
+        print("Sparcity:"), sparcity
         if sparcity >= 0.5:
             self._model.sparsify()
 
