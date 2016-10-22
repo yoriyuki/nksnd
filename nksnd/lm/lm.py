45ffbe3cb0a39fce0065f7179ab82000ea565b7a
yoriyuki <yoriyuki.y@gmail.com>
2016-10-23 04:05:03 +0900

    Toplevel language model

---
+++ nksnd/lm/lm.py
@@ -0,0 +1,15 @@
+import collocation_lm
+
+class LM:
+
+    def __init__(self):
+        self._collocationLM = CollocationLM()
+
+    def train(self, file_names):
+        pass
+
+    def save(self, path):
+        pass
+
+    def load(self, path):
+        pass
