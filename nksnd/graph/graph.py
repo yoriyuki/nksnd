fd5a81581558defce57acf416955b85f8e16a037
yoriyuki <yoriyuki.y@gmail.com>
2016-10-24 20:54:59 +0900

    Building graph

--- nksnd/graph/graph.py
+++ nksnd/graph/graph.py
@@ -13,8 +13,22 @@ class EOS(Node):
 
 class Graph:
     def __init__(self, dict, string):
-        graph = [[] for i in range(1 + len(s))]
-        graph[0].append(BOS())
-        graph[1 + len(s)].append(EOS(1 + len(s)))
+        graph = []
+        graph.append([BOS()])
 
-        
+        for i in range(len(string)):
+            sub = string[i:-1]
+            nodes = [Node(i, word) for word in dict.iter_prefixes(sub)]
+            if nodes == []:
+            for j in range(i, len(string)):
+                if dict.prefixes(string[j:-1]) != []:
+                    s = string[i:j]
+                    nodes = [
+                        Node(i, literal_word(s)),
+                        Node(i, katakana_word(s)),
+                        Node(i, latin_word(s))
+                    ]
+                    break
+            graph.append(nodes)
+
+        graph.append(EOS(1 + len(s)))
