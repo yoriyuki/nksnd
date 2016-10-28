4cdfc7ba7ef37e3134d7ddf70491e3aeb055fb67
yoriyuki <yoriyuki.y@gmail.com>
2016-10-28 23:13:12 +0900

    beta

--- nksnd/graph/forward_backward.py
+++ nksnd/graph/forward_backward.py
@@ -2,11 +2,23 @@ from utils import numerics
 
 def set_log_alpha(unigram_weight, bigram_weight, graph):
     for last_pos in range(graph.x_length):
-        nodes = graph.nodes_list[last_pos]
-        for node in nodes:
+        for node in graph.nodes_list[last_pos]:
             prev_nodes = graph_nodes_list[node.start_pos]
-            log_alpha = unigram_weight.get(node)
+            node.log_alpha = unigram_weight.get(node)
             for prev_node in prev_nodes:
                 w = bigram_weight.get(prev_node, node)
-                log_alpha = numerics.logsumexp(log_alpha, w)
-            node.log_alpha = log_alpha
+                node.log_alpha =
+                    numerics.logsumexp(node.log_alpha, prev_node.log_alphe + w)
+
+def set_log_beta(unigram_weight, bigram_weight, graph):
+    for last_pos in range(graph.x_length):
+        for node in graph.nodes_list[last_pos]:
+            node.log_beta = unigram_weight.get(node)
+
+    for last_pos in range(graph.x_length, 0, -1):
+        for node in graph.nodes_list[last_pos]:
+            prev_nodes = graph_nodes_list[node.start_pos]
+            for prev_node in prev_nodes:
+                w = bigram_weight.get(prev_node, node)
+                prev_node.log_beta =
+                    numerics.logsumexp(prev_node.log_beta, node.log_beta + w)
