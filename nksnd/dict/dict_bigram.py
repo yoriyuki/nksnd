b2ae279195e4b00c5ae89f6ca05cacce7c775829
Yoriyuki Yamagata <yoriyuki.y@gmail.com>
2016-11-02 17:47:30 +0900

    dictionary update

--- nksnd/dict/dict_bigram.py
+++ nksnd/dict/dict_bigram.py
@@ -1,29 +1,37 @@
 import marisa_trie
-import words
+from utils import numerics, words
 
 class DictBigram:
     def __init__(self, known_words):
         self._dict = {}
         self._normalized_count = {}
         self._known_words = known_words
+        self._count = 0
 
     def get(self, word1, word2):
         w1 = words.replace_word(self._known_words, word1)
         w2 = words.replace_word(self._known_words, word2)
-        if (w1, w2) in self._dict:
-            return self._dict[(w1, w2)]
+        k = words.compose_bigram_key(w1, w2)
+        if k in self._dict:
+            return self._dict[k]
         else:
             return 0
 
-    def set(self, word1, word2, weight):
-        w1 = words.replace_word(self._known_words, word1)
-        w2 = words.replace_word(self._known_words, word2)
-        self._dict[(w1, w2)] = weight
+    def update(self, g):
+        self._count += 1
+        for key in g.dict.keys():
+            if word.is_bigram_key(key):
+                if key in self._normalized_count:
+                    d = self._normalized_count[key]
+                else:
+                    d = 0
+                w = numerics.clip(self._dict[key], lmconfig.normalization_factor * d)
+                self._dict[key] = self._dict[key] + lmconfig.eta * g.get(key)
+                self._normalized_count[k] = self._count
 
     def save(self, path):
-        items = ((ws[0] + ' ' + ws[1], score) for ws, score in self._dict.iteritems())
         fmt ="<f"
-        trie = marisa_trie.RecordTrie(fmt, items)
+        trie = marisa_trie.RecordTrie(fmt, self._dict.iteritems())
         dict_filename = os.path.join(path, 'bigram_dict')
         with open(dict_filename, 'w') as file:
             trie.write(f)
--- nksnd/utils/sparse_vector.py
+++ nksnd/utils/sparse_vector.py
@@ -19,14 +19,6 @@ class SparseVector:
             self.set(key) = numerics.logsumexp(self.get(key), sv.get(key))
         return self
 
-    def __iadd__(self, sv):
-        for key, val in sv.iteritems():
-            if key in self._dict:
-                self.dict[key] = self.dict[key] + val
-            else:
-                self.dict[key] = val
-        return self
-
     def minusexp(self, sv):
         for key in sv.dict:
             self.set(key) = self.get(key) - math.exp(sv.get(key))
