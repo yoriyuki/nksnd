2d28ae7ee29010060dd922a96adfc6f1813d6be4
Yoriyuki Yamagata <yoriyuki.y@gmail.com>
2017-01-05 13:11:06 +0900

    Hyper parameters

--- nksnd/config/lmconfig.py
+++ nksnd/config/lmconfig.py
@@ -2,6 +2,7 @@ from config import parallel_config
 
 iteration = 5
 gaussian = 0
+tolerance = 1e-20
 unknownword_threshold = 1
 eta = 0.1
 regularization_factor = eta * 0.0001 * parallel_config.chunk_size * parallel_config.processes
